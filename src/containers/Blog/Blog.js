c80395e5686cc6ca9ecb96c5593ea6ce2e5dc95d
Saurabh Kumar <saurabh.cse04@gmail.com>
2020-05-10 11:28:03 +0530

    🏬

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,20 +7,30 @@ import './Blog.css';
 import axios from 'axios'
 
 class Blog extends Component {
+    state = {
+        posts : []
+    }
+
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(response =>{
-            console.log("===",response);
+            const posts = response.data.slice(0,4);
+            const updatedPost = posts.map(post =>{
+                return {...post, author: 'Saurabh'}
+            }) 
+            this.setState({posts: updatedPost})
+            // console.log("===",response);
         })
     }
     
     render () {
+        const posts = this.state.posts.map(post =>{
+            return <Post key = {post.id} title = {post.title} author ={post.author} />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />

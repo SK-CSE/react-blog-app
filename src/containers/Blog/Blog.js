40795e89e0619c08b48c925d39e4acaaf5ca18ee
Saurabh Kumar <saurabh.cse04@gmail.com>
2020-05-10 16:50:23 +0530

    🌘

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from 'axios'
 
 class Blog extends Component {
     state = {
-        posts : []
+        posts : [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -22,10 +23,17 @@ class Blog extends Component {
             // console.log("===",response);
         })
     }
+    postSelectedHandler = (id) =>{
+        this.setState({selectedPostId: id})
+    }
     
     render () {
         const posts = this.state.posts.map(post =>{
-            return <Post key = {post.id} title = {post.title} author ={post.author} />
+            return <Post 
+                key = {post.id} 
+                title = {post.title} 
+                author ={post.author} 
+                clicked = {()=> this.postSelectedHandler(post.id)}/>
         })
         return (
             <div>
@@ -33,7 +41,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
